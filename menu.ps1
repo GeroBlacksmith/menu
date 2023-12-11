916640989240b959ffd8b3a710cff7df4ccc6d84
Gerardo Herrera <gerardoherrera87@yahoo.com.ar>
2023-12-10 21:25:02 -0300

    Added opcion 2

--- menu.ps1
+++ menu.ps1
@@ -1,20 +1,29 @@
 ﻿do {
     Write-Host "1. Navegar al directorio del proyecto"
+    Write-Host "2. Navegar al directorio de trabajo"
     Write-Host "0. Salir"
-
+    $exit = "no"
     $input = Read-Host "Por favor, elige una opción"
+    $paths = Get-Content 'config.txt'
     switch ($input) {
         '1' {
-            $path = Get-Content 'config.txt'
-            Set-Location -Path $path
+            Set-Location -Path $paths[0]
             Write-Host (Get-Location).Path
+            break
+        }
+        '2' {
+            Set-Location -Path $paths[1]
+            Write-Host $paths[1]
+            break
+          
         }
         '0' {
             Write-Host "Saliendo..."
+            $exit = "yes"
             break
         }
         default {
             Write-Host "Opción no válida. Por favor, intenta de nuevo."
         }
     }
-} while ($input -ne '0')
+} while ($exit -ne 'yes')
