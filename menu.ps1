644021ba661e95201ef26dd858d0cd6f3b34a4e6
Gerardo Herrera <gerardoherrera87@yahoo.com.ar>
2023-12-10 21:04:31 -0300

    primera version del menu

---
+++ menu.ps1
@@ -0,0 +1,19 @@
+﻿do {
+    Write-Host "1. Navegar a path/to/my/projects"
+    Write-Host "0. Salir"
+
+    $input = Read-Host "Por favor, elige una opción"
+    switch ($input) {
+        '1' {
+            Set-Location -Path 'C:\Users\Gerardo\Proyectos'
+            Write-Host (Get-Location).Path
+        }
+        '0' {
+            Write-Host "Saliendo..."
+            break
+        }
+        default {
+            Write-Host "Opción no válida. Por favor, intenta de nuevo."
+        }
+    }
+} while ($input -ne '0')
